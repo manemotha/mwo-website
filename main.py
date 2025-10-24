71b6ee3d42a8b1f7670353df9607df01546baa04
manemotha <mane.motha@proton.me>
2025-10-24 14:56:30 +0200

    update uvicorn host to allow external access

--- main.py
+++ main.py
@@ -114,4 +114,4 @@ async def new_article(title: str = Form(...), body: str = Form(...)):
     return RedirectResponse(url="/admin", status_code=303)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
