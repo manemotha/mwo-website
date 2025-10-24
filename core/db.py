a1dcf5120da6c916956e54250fcbe1a0c147530d
manemotha <mane.motha@proton.me>
2025-10-24 17:14:59 +0200

    refactor database access in post functions to ensure TinyDB is closed after operations

--- core/db.py
+++ core/db.py
@@ -2,35 +2,39 @@ import datetime
 from tinydb import TinyDB, Query
 from core.formatters import iso_format_datetime
 
-db = TinyDB('db.json')
 
 # Add new post to the database
 def add_post(title: str, body: str):
+    db = TinyDB('db.json')
     posts_table = db.table('posts')
     posts_table.insert({
         'title': title.upper(),
         'body': body,
         'date': str(iso_format_datetime(datetime.datetime.now())),
     })
+    db.close()
 
 # Get all posts from the database
 def get_posts() -> list:
+    db = TinyDB('db.json')
     posts_table = db.table('posts')
     posts = posts_table.all()
+    db.close()
     if isinstance(posts, list) and len(posts) > 0:
         # Sort posts by date in descending order
         posts.sort(key=lambda x: x['date'], reverse=True)
         for post in posts:
             post['title'] = post['title'].title()
         return posts
-
     raise ValueError("No posts found in the database.")
 
 # Get a single post by title
 def get_post_by_title(title: str) -> dict:
+    db = TinyDB('db.json')
     posts_table = db.table('posts')
     post = Query()
     result = posts_table.search(post.title == title.upper())
+    db.close()
     if result:
         article = result[0]
         article['title'] = article['title'].title()
